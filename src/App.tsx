24b84706cf540c265525f8d7c3e215f3882cac46
huanc0de <arturfurtak5@gmail.com>
2024-02-12 19:46:48 +0100

    added some variables

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,26 @@ function App() {
       condition: {
         icon: '',
       },
+      feelslike_c: 0,
+      wind_kph: 0,
+      humidity: 0,
+    },
+    forecast: {
+      forecastday: [
+        {
+          day: {
+            maxtemp_c: 0,
+            mintemp_c: 0,
+          },
+        },
+      ],
     },
   });
   const [query, setQuery] = useState('');
   const URL: string = `https://api.weatherapi.com/v1/forecast.json?key=${
     import.meta.env.VITE_KEY
   }&q=${query}&days=1&aqi=yes&alerts=no`;
+  const date = new Date();
 
   const fetchData = async (URL: string) => {
     const res = await fetch(URL);
@@ -33,6 +47,19 @@ function App() {
           condition: {
             icon: data.current.condition.icon,
           },
+          feelslike_c: data.current.feelslike_c,
+          wind_kph: data.current.wind_kph,
+          humidity: data.current.humidity,
+        },
+        forecast: {
+          forecastday: [
+            {
+              day: {
+                maxtemp_c: data.forecast.forecastday[0].day.maxtemp_c,
+                mintemp_c: data.forecast.forecastday[0].day.mintemp_c,
+              },
+            },
+          ],
         },
       });
       console.log(data);
@@ -57,8 +84,18 @@ function App() {
           value={query}
           onChange={(e) => handleQueryChange(e)}
         />
-        <p>{data.location.name}</p>
-        <p>{data.current.temp_c}</p>
+        <p>Name {data.location.name}</p>
+        <p>
+          Date
+          {String(date.toLocaleString('default', { day: '2-digit' })) +
+            String(date.toLocaleString('default', { month: 'short' }))}
+        </p>
+        <p>Current {data.current.temp_c}</p>
+        <p>Max {data.forecast.forecastday[0].day.maxtemp_c}</p>
+        <p>Min {data.forecast.forecastday[0].day.mintemp_c}</p>
+        <p>Feels {data.current.feelslike_c}</p>
+        <p>Wind speed {data.current.wind_kph}</p>
+        <p>humidity {data.current.humidity}%</p>
         <img src={data.current.condition.icon} alt="" />
       </div>
     </>
